3c97d10ebd8e674957bc6d334746886bb643b62b
Chirag Pipaliya <chiragpipaliya454@gmail.com>
2020-03-15 16:38:51 +0530

    added user,post edit delete modules to project

--- controller/admin.js
+++ controller/admin.js
@@ -1,6 +1,7 @@
 const Post = require("../models/post");
 const User = require("../models/user");
 const mongoose = require("mongoose");
+const bcryptjs = require("bcryptjs");
 
 exports.getDashboard = async (req, res) => {
   const totPost = await Post.find({}).count();
@@ -47,4 +48,85 @@ exports.postAddPost = async (req, res) => {
     console.log(e);
     res.send(e);
   }
+};
+exports.getEditPost = async (req, res) => {
+  // const id = mongoose.Types.ObjectId(req.params.id);
+  const post = await Post.findById(req.params.name);
+  if (!post) {
+    return res.status(404).redirect("/404");
+  }
+  res.render("admin/editpost", {
+    post
+  });
+};
+
+exports.postEditPost = async (req, res) => {
+  const id = req.body.id;
+  const post = await Post.findByIdAndUpdate(id, {
+    title: req.body.title,
+    imgUrl: req.body.imgUrl,
+    body: req.body.body,
+    keywords: req.body.keywords
+  });
+  await post.save();
+  res.redirect("/admin/posts");
+};
+
+exports.getDeletePost = async (req, res) => {
+  const id = req.params.id;
+  const post = await Post.findById(id);
+  if (!post) {
+    return res.status(404).redirect("/404");
+  }
+
+  await Post.findByIdAndRemove(id);
+  res.redirect("/admin/posts");
+};
+
+exports.getAllUsers = async (req, res) => {
+  const users = await User.find({});
+  res.render("admin/users", { users });
+};
+exports.getEditUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    return res.status(404).redirect("/404");
+  }
+  res.render("admin/edituser", { user });
+};
+exports.postEditUser = async (req, res) => {
+  const id = req.body.id;
+  const u = await User.findById(id);
+  if (!u) {
+    return res.status(404).redirect("/404");
+  }
+  const user = await User.findByIdAndUpdate(id, {
+    email: req.body.email,
+    role: req.body.role
+  });
+  await user.save();
+  res.redirect("/admin/users");
+};
+
+exports.postChangePassword = async (req, res) => {
+  const id = req.body.id;
+  const u = await User.findById(id);
+  if (!u) {
+    return res.status(404).redirect("/404");
+  }
+  const password = await bcryptjs.hash(req.body.password, 10);
+  const user = await User.findByIdAndUpdate(id, { password: password });
+  await user.save();
+  res.redirect("/admin/users");
+};
+
+exports.getDeleteUser = async (req, res) => {
+  const id = req.params.id;
+  const u = await User.findById(id);
+  if (!u) {
+    return res.status(404).redirect("/404");
+  }
+
+  await User.findByIdAndRemove(id);
+  res.redirect("/admin/users");
 };
--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -4,9 +4,27 @@ const admin = require("../../controller/admin");
 const { ensureAuthenticated, isAdmin } = require("../../controller/authRoles");
 
 // router.get("/", ensureAuthenticated, isAdmin, admin.getDashboard);
-router.get("/", admin.getDashboard);
+router.get("/", ensureAuthenticated, isAdmin, admin.getDashboard);
 router.get("/addpost", ensureAuthenticated, isAdmin, admin.getAddPost);
 router.get("/posts", ensureAuthenticated, isAdmin, admin.getAllPost);
 router.post("/addpost", ensureAuthenticated, isAdmin, admin.postAddPost);
+router.post("/edit", ensureAuthenticated, isAdmin, admin.postEditPost);
+router.post("/user/edit", ensureAuthenticated, isAdmin, admin.postEditUser);
+router.post(
+  "/user/edit/changepass",
+  ensureAuthenticated,
+  isAdmin,
+  admin.postChangePassword
+);
+router.get("/users", ensureAuthenticated, isAdmin, admin.getAllUsers);
+router.get("/delete/:id", ensureAuthenticated, isAdmin, admin.getDeletePost);
+router.get("/user/edit/:id", ensureAuthenticated, isAdmin, admin.getEditUser);
+router.get(
+  "/user/delete/:id",
+  ensureAuthenticated,
+  isAdmin,
+  admin.getDeleteUser
+);
+router.get("/edit/:name", ensureAuthenticated, isAdmin, admin.getEditPost);
 
 module.exports = router;
