93121d40b18b819962ed65f511342cf24f3cd679
Chirag Pipaliya <chiragpipaliya454@gmail.com>
2020-03-18 12:29:42 +0530

    Added user operations - user can add post - user can see their post list - user can manage their post - user can edit,delete their post

--- controller/home.js
+++ controller/home.js
@@ -40,3 +40,90 @@ exports.getPostPage = async (req, res) => {
     isLogin: req.user ? true : false
   });
 };
+
+exports.getAddPost = async (req, res) => {
+  res.render("addpost", {
+    isLogin: req.user ? true : false
+  });
+};
+
+exports.postAddPost = async (req, res) => {
+  const title = req.body.title;
+  const urlTitle = title.replace(/ /g, "-");
+  const imgUrl = req.body.imgUrl;
+  const body = req.body.body;
+  const keywords = req.body.keywords;
+  // console.log(body);
+  // console.log(req.body);
+  try {
+    const post = new Post({
+      title,
+      urlTitle,
+      imgUrl,
+      body,
+      keywords,
+      owner: req.user._id
+    });
+    await post.save();
+    res.redirect("/user/posts");
+  } catch (e) {
+    console.log(e);
+    res.send(e);
+  }
+};
+
+exports.getAllPost = async (req, res) => {
+  // const uid = mongoose.Types.ObjectId(req.user._id);
+  const posts = await Post.find({ owner: req.user._id });
+
+  res.render("posts", {
+    posts,
+    isLogin: req.user ? true : false
+  });
+};
+
+exports.getEditPost = async (req, res) => {
+  // const id = mongoose.Types.ObjectId(req.params.id);
+  const post = await Post.findById(req.params.id);
+  console.log(!(post.owner.toString() === req.user._id.toString()));
+  if (!(post.owner.toString() === req.user._id.toString())) {
+    return res.status(404).redirect("/404");
+  }
+  if (!post) {
+    return res.status(404).redirect("/404");
+  }
+  res.render("editpost", {
+    post,
+    isLogin: req.user ? true : false
+  });
+};
+
+exports.postEditPost = async (req, res) => {
+  const id = req.body.id;
+  const upost = await Post.findById(id);
+  if (!(upost.owner.toString() === req.user._id.toString())) {
+    return res.status(404).redirect("/404");
+  }
+  const post = await Post.findByIdAndUpdate(id, {
+    title: req.body.title,
+    imgUrl: req.body.imgUrl,
+    body: req.body.body,
+    keywords: req.body.keywords
+  });
+  await post.save();
+  res.redirect("/user/posts");
+};
+
+exports.getDeletePost = async (req, res) => {
+  const id = req.params.id;
+  const post = await Post.findById(id);
+  if (!(post.owner.toString() === req.user._id.toString())) {
+    return res.status(404).redirect("/404");
+  }
+  if (!post) {
+    return res.status(404).redirect("/404");
+  }
+
+  await Post.findByIdAndRemove(id);
+  res.redirect("/user/posts");
+};
--- routes/home/home.js
+++ routes/home/home.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const home = require("../../controller/home");
+const { ensureAuthenticated, isAdmin } = require("../../controller/authRoles");
 
 router.get("/", home.getHomePage);
+router.get("/user/addpost", ensureAuthenticated, home.getAddPost);
+router.post("/user/addpost", ensureAuthenticated, home.postAddPost);
+router.get("/user/posts", ensureAuthenticated, home.getAllPost);
+router.get("/user/post/edit/:id", ensureAuthenticated, home.getEditPost);
+router.post("/user/post/edit", ensureAuthenticated, home.postEditPost);
+router.get("/user/post/delete/:id", ensureAuthenticated, home.getDeletePost);
 router.get("/post/:name", home.getPostPage);
 
 module.exports = router;
